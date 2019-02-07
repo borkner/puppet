077a87fe1835568ba33cc549e23e82c66b4f57bf
Borkner <borkner@web.de>
2019-02-07 09:48:05 +0100

    alten file config gelöscht

--- manifests/ntp.pp
+++ manifests/ntp.pp
@@ -1,8 +1,4 @@
-# Set up regular Puppet runs
-file { '/usr/local/bin/run-puppet':
-  source => '/etc/puppetlabs/code/environments/production/files/run-puppet.sh',
-  mode   => '0755',
-}
+# Set up NTP
 
 package { 'ntp':
 	ensure => installed,
