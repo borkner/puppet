cd74abcb7b98162d255aecaaf5aaf70072d1ac70
Borkner <borkner@web.de>
2019-02-07 09:39:52 +0100

    NTP Service installieren

---
+++ manifests/ntp.pp
@@ -0,0 +1,9 @@
+# Set up regular Puppet runs
+file { '/usr/local/bin/run-puppet':
+  source => '/etc/puppetlabs/code/environments/production/files/run-puppet.sh',
+  mode   => '0755',
+}
+
+package { 'ntp':
+	ensure => installed,
+}
